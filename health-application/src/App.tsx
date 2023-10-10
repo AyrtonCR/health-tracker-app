9743464e8e80adae654df8fbc35de6f443958171
AyrtonCR <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2023-10-10 15:49:38 +1300

    working on navbar, need to add responsive menu for mobile

--- health-application/src/App.tsx
+++ health-application/src/App.tsx
@@ -2,16 +2,17 @@
 // import Carousela from "./components/carousela.tsx";
 import Navbar from "@/scenes/navbar";
 import Footer from "./components/footer";
-import { Collapse, Dropdown, Ripple, Carousel, initTE } from "tw-elements";
-initTE({ Collapse, Dropdown, Ripple, Carousel });
+import { useState } from "react";
+import SelectedPage from "@/shared/types";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
   return (
     <>
-      <Navbar />
-      <div className="bg-green-300">
-        <p>shit</p>
-      </div>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="app bg-gray20"></div>
       {/* <Carousela />    This did not work, JS Problem */}
       <Footer />
     </>
--- health-application/src/scenes/navbar/index.tsx
+++ health-application/src/scenes/navbar/index.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./link";
+import SelectedPage from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
-type Props = object;
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = () => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -14,18 +24,48 @@ const Navbar = () => {
             {/* Left Side */}
             <img alt="Logo" src={Logo} />
             {/* Right side */}
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Classes</p>
-                <p>Contact Us</p>
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a member
+                  </ActionButton>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a member</button>
+            ) : (
+              <div>
+                <button
+                  className="rounded-full bg-secondary-500 p-2"
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
+                >
+                  <Bars3Icon className="h-6 w-6 text-white" />
+                </button>
               </div>
-            </div>
+            )}{" "}
           </div>
         </div>
       </div>
