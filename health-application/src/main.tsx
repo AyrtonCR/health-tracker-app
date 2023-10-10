d80c9ee778bb30fbb34d377a65d5bacf75ad5c00
AyrtonCR <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2023-10-10 14:04:25 +1300

    fixed prettier typescript formating ES error, starting on navbar

--- health-application/src/main.tsx
+++ health-application/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// Initialization for ES Users
+import { Collapse, Dropdown, Ripple, Carousel, initTE } from "tw-elements";
+initTE({ Collapse, Dropdown, Ripple, Carousel });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
