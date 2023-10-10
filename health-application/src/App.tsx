e52b57d027d8941d994c0f5cb7ce334907c7e431
AyrtonCR <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2023-10-10 16:13:55 +1300

    navbar complete for now, need to add transition styling and close when click off at later date

--- health-application/src/App.tsx
+++ health-application/src/App.tsx
@@ -2,18 +2,34 @@
 // import Carousela from "./components/carousela.tsx";
 import Navbar from "@/scenes/navbar";
 import Footer from "./components/footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SelectedPage from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true), setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       <div className="app bg-gray20"></div>
-      {/* <Carousela />    This did not work, JS Problem */}
       <Footer />
     </>
   );
