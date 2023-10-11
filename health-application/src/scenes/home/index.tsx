a48464abf2822b3b8ed9c7f8db074b9ae2247f0a
AyrtonCR <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2023-10-11 14:43:27 +1300

    header and basic content complete

--- health-application/src/scenes/home/index.tsx
+++ health-application/src/scenes/home/index.tsx
@@ -8,6 +8,7 @@ import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import SelectedPage from "@/shared/types";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -22,25 +23,49 @@ function Home({ setSelectedPage }: Props) {
       className="gap-16 bg-gray-20 pb-10 pt-10 md:h-full md:pb-0"
     >
       {/* Image and Main Headxer */}
-      <div>
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* Main Header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <div>
-            <div>
-              <div>
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              {/* cspell: disable-next-line */}
+              <div className="before:absolute before:-top-20 before:left-20 before:z-[-1] before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
-            <p>
+            <p className="mt-8 text-sm">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
               sapiente rerum odit nemo culpa neque at enim! Sequi, iste? Dolores
               vitae delectus voluptate incidunt, possimus temporibus laborum
               odit repellat alias?
             </p>
-          </div>
+          </motion.div>
           {/* Actions */}
-          <div>
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -51,19 +76,19 @@ function Home({ setSelectedPage }: Props) {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         {/* Image */}
-        <div>
+        <div className="z-10 flex basis-3/5 justify-center md:ml-40 md:mt-16 md:justify-end">
           <img alt="home-page-graphic" src={HomePageGraphic}></img>
         </div>
-      </div>
+      </motion.div>
       {/* Sponsors */}
 
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h=[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img alt="redbull-sponsor" src={SponsorRedbull}></img>
               <img alt="forbes-sponsor" src={SponsorForbes}></img>
               <img alt="fortune-sponsor" src={SponsorFortune}></img>
