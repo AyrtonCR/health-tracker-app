20ade067c5e596b5462f6d340e4a412d5f3ba2ab
AyrtonCR <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2023-12-11 18:06:28 +1300

    added more dummy data

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,14 @@ const foodModel = require("../models/foodModel");
 /* GET home page. */
 
 router.get("/", function (req, res, next) {
-  const foods = foodModel.find();
-  const foodsTestData = [{ Apple: 380 }];
+  // const foods = foodModel.find();
+  const foodsTestData = [{ Apple: 380 }, { Banana: 200 }, { Toast: 300 }, { Chicken: 500 }];
+
+  const foodTotal = arr.reduce((accumulator, object) => {
+    return accumulator + object.salary;
+  }, 0);
+
+  console.log(foodTotal);
 
   return res.status(200).json(foodsTestData);
 });
